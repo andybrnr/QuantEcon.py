6bdbebd0054759b1a924da5571075a49800d757f
andybrnr <andy.berner@gmail.com>
2015-07-10 11:51:12 -0700

    update scratch and exercise scripts

---
+++ lecture scratch scripts/QuantEcon_Exercises_Ch2Sec3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 09 15:48:14 2015
+
+@author: ABerner
+"""
+
+from __future__ import print_function
+
+import numpy as np
+import scipy as sp
+import scipy.stats as stats
+import quantecon as qe
+import matplotlib.pyplot as plt
+
+#Ex 1
+
